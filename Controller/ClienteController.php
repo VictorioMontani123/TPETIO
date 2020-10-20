e77d1e90171aa9f249c022808dcb6947b345f351
Jeremias Lescano <jerelescano1@gmail.com>
2020-10-20 19:14:54 -0300

    redireccion en los botones

--- Controller/ClienteController.php
+++ Controller/ClienteController.php
@@ -19,21 +19,21 @@
 
     public function Alta(){
         $this->model->Insert($_POST['id'],$_POST['nombre'],$_POST['apellido'],$_POST['direccion'],$_POST['email'],$_POST['telefono'],$_POST['cuit']);
-        $this->view->ShowHome();
+        $this->view->ShowAll();
     }
 
     public function delete($id = null){
         $id = $id[':ID'];
         $this->model->Delete($id);
 
-        $this->view->ShowHome();
+        $this->view->ShowAll();
 
     }
 
     public function Modificar($id = null){
          $this->model->Edit($_POST['id'],$_POST['nombre'],$_POST['apellido'],$_POST['direccion'],$_POST['email'],$_POST['telefono'],$_POST['cuit']);
          
-         $this->view->ShowHome();
+         $this->view->ShowAll();
     }
 
 
